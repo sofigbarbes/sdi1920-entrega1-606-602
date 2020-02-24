8a84d3f1856c82065ddeff134f959087e2d04338
UO263909 <UO263909@uniovi.es>
2020-02-24 15:42:45 +0100

    Creación inicial

---
+++ sdi1920-entrega1-606-602/src/test/java/com/socialNetwork/Sdi1920Entrega1606602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920Entrega1606602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
