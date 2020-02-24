a0c0e6aabc34d150e550811c5a212943ba671c96
UO263909 <UO263909@uniovi.es>
2020-02-24 15:49:07 +0100

    package structure created

--- sdi1920-entrega1-606-602/src/main/java/com/socialNetwork/Sdi1920Entrega1606602Application.java
+++ sdi1920-entrega1-606-602/src/main/java/com/socialNetwork/Sdi1920Entrega1606602Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sdi1920Entrega1606602Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(Sdi1920Entrega1606602Application.class, args);
 	}
 
