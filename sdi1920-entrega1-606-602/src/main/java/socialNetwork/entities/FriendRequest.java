83df99b16c14cb5e4cdb9885dca50604110c16e4
blluci <uo264883@uniovi.es>
2020-03-16 15:11:54 +0100

    Fixed warnings

--- sdi1920-entrega1-606-602/src/main/java/socialNetwork/entities/FriendRequest.java
+++ sdi1920-entrega1-606-602/src/main/java/socialNetwork/entities/FriendRequest.java
@@ -10,6 +10,11 @@ import socialNetwork.services.FriendRequestID;
 @Entity
 public class FriendRequest implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	FriendRequestID id;
 
--- sdi1920-entrega1-606-602/src/main/java/socialNetwork/services/FriendRequestID.java
+++ sdi1920-entrega1-606-602/src/main/java/socialNetwork/services/FriendRequestID.java
@@ -6,6 +6,10 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class FriendRequestID implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String senderEmail;
 	String receiverEmail;
 	
